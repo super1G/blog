dd328678899879490b09042be5877ed02acf80f3
busydog1G <stu112114@gmail.com>
2020-09-03 17:55:23 +0800

    增加欄位 name

--- lin1/blog/database/seeds/DatabaseSeeder.php
+++ lin1/blog/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         DB::table('users')->insert([
-            
+            'name' => str_random(5),
             'email' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
             'api_token' =>str_random(10),
