fe8a8d43e15d56e98e5fa378ccf1e65d0712ad34
busydog1G <stu112114@gmail.com>
2020-09-03 16:57:55 +0800

    seeder 自動生成 user資料表 cmd   : php artisan db:seed

--- lin1/blog/database/seeds/DatabaseSeeder.php
+++ lin1/blog/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+            'api_token' =>str_random(10),
+        ]);
     }
 }
