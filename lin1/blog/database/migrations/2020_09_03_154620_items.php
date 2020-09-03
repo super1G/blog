4469d69970c47c933ca3598705482dfd35e0625c
busydog1G <stu112114@gmail.com>
2020-09-03 16:11:47 +0800

    建立使用者資料表以及商品資料表

--- lin1/blog/database/migrations/2020_09_03_154620_items.php
+++ lin1/blog/database/migrations/2020_09_03_154620_items.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Items extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('item', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('desc')->nullable();
+            $table->integer('price')->default(0)->unsigned();
+           
+            $table->boolean('enable')->default(true);
+            
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('item');
     }
 }
